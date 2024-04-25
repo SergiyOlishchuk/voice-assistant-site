cb80fc853999fe1a2ac38c14c8bc2bd353c5441c
Sergiy Olishchuk <olishchyk.sergiy@gmail.com>
2024-04-25 21:38:20 +0300

    Changed users view in admin panel

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,28 @@ from django.contrib import admin
 from users.models import User
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email"]
+
+    search_fields = ["username", "first_name", "last_name", "email"]
+
+    fields = [
+        "username",
+        ("first_name", "last_name"),
+        "email",
+        "openAI_token",
+        "image",
+        ("date_joined", "last_login"),
+        ("is_staff", "is_active", "is_superuser"),
+        ("groups", "user_permissions"),
+    ]
+
+    readonly_fields = [
+        "date_joined", "last_login",  "is_superuser", 
+    ]
+    
+    list_filter = ['is_staff', 'is_active']
--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.contrib import auth
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
